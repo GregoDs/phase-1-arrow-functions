48a99f19a55eebe3715d639d8f0a3ffd8a639139
GregoDs <gregorykago007@gmail.com>
2024-01-20 16:11:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(parameter1, parameter2){
+parameter1 = 2000;
+parameter2 =100;
+return parameter1 / parameter2;
+};
+
+const square = parameter => parameter * parameter;
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
